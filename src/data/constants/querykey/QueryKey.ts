383124a3a9c52ee349d6777a6d5840d328870069
Rory0304 <mitty0304@naver.com>
2025-11-27 19:02:32 +0900

    feat: add query keys

---
+++ src/data/constants/querykey/QueryKey.ts
@@ -0,0 +1,11 @@
+export const QueryKey = {
+  quoteCard: {
+    get_quotecrad: "get_quotecrad",
+    get_quotecard_list: "get_quotecard_list",
+    get_my_quotecard_list: "get_my_quotecard_list",
+    create_quotecard: "create_quotecard",
+  },
+  image: {
+    upload_image: "upload_image",
+  },
+} as const;
