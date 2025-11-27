a93b71f1b53ecd274d4f79ffcb4e24b7311e3c8b
Rory0304 <mitty0304@naver.com>
2025-11-27 17:33:36 +0900

    feat: add editor layout

---
+++ src/app/editor/layout.tsx
@@ -0,0 +1,30 @@
+import { Noto_Serif_KR, Nanum_Pen_Script } from "next/font/google";
+
+// Editor에서만 사용할 폰트들
+const notoSerifKR = Noto_Serif_KR({
+  variable: "--font-editor-serif",
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  display: "swap",
+});
+
+const nanumPenScript = Nanum_Pen_Script({
+  variable: "--font-editor-handwriting",
+  subsets: ["latin"],
+  weight: ["400"],
+  display: "swap",
+});
+
+export default function EditorLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div
+      className={`${notoSerifKR.variable}  ${nanumPenScript.variable} h-screen overflow-scroll`}
+    >
+      {children}
+    </div>
+  );
+}
