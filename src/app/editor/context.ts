24e0a38f693b28c3cbfdcbe70b6b2be584a15b2c
Rory0304 <mitty0304@naver.com>
2025-11-27 17:15:20 +0900

    feat: refactor editor context and reducer to support new data structure and enhanced state management

--- src/app/editor/context.ts
+++ src/app/editor/context.ts
@@ -1,87 +1,98 @@
 import { createContext, RefObject } from "react";
 import { BackgroundColor } from "@/data/constants/editor/BackgroundColor";
-import { EditorBackgroundColorType } from "@/data/interfaces/editor/EditorBackgroundColor";
-import { FontSize, FontSizeType } from "@/data/constants/editor/FontSize";
-import { TextAlign, TextAlignType } from "@/data/constants/editor/TextAlign";
-import { FontFamily, FontFamilyType } from "@/data/constants/editor/FontFamily";
-import {
-  AspectRatio,
-  AspectRatioType,
-} from "@/data/constants/editor/AspectRatio";
+import { TextAlign } from "@/data/constants/editor/TextAlign";
+import { FontFamily } from "@/data/constants/editor/FontFamily";
+import { AspectRatio } from "@/data/constants/editor/AspectRatio";
 import { nanoid } from "nanoid";
-import { QuoteCardHandler } from "@/components/pages/editor/element/QuoteCard";
 
-/**
- * 선택된 element 상태값
- */
-export interface EditorState {
-  selectedElement: EditorElement;
-}
+import { EditorElement } from "@/data/interfaces/editor/EditorElement";
+import { EditorConfig } from "@/data/interfaces/editor/EditorConfig";
+import { Layout } from "@/data/interfaces/editor/EditorLayout";
+import { EditorData } from "@/data/interfaces/editor";
+import { EditorState } from "@/data/interfaces/editor/EditorState";
+import { Editor } from "@/data/interfaces/editor/Editor";
+import { EditorTemplate } from "@/data/interfaces/editor/EditorTemplate";
 
-/**
- * 기본 정보
- */
-export interface EditorConfig {
-  title: string;
-}
-
-/**
- * 개별 element
- */
-export interface EditorElement {
-  id: string;
-
-  content: string;
-  author: string; // [TODO] Reference 로 변수명 수정
-
-  // [TODO] BackgroundColor 하나의 필드로 합칠 수 있는 방법이 있을지 확인
-  customBackgroundColor?: string;
-  backgroundColor?: EditorBackgroundColorType;
-
-  fontSize: FontSizeType;
-  textAlign: TextAlignType;
-  fontFamily: FontFamilyType;
-  aspectRatio: AspectRatioType;
-}
-
-export interface Editor {
-  config: EditorConfig;
-  state: EditorState;
-  elements: EditorElement[];
-}
-
-/**
- * [TODO] update 로직은 모두 partial 로 바꾼다.
- */
 export type EditorAction =
+  | {
+      type: "INIT_STATE";
+      payload: Editor;
+    }
   | {
       type: "UPDATE_CONFIG";
       payload: Partial<EditorConfig>;
     }
   | {
+      type: "UPDATE_TEMPLATE";
+      payload: {
+        template: Partial<EditorTemplate>;
+      };
+    }
+  | {
+      type: "UPDATE_LAYOUT";
+      payload: {
+        layout: Partial<Layout>;
+      };
+    }
+  | {
       type: "UPDATE_ELEMENT";
-      payload: Partial<EditorElement>;
+      payload: {
+        element: EditorElement;
+      };
+    }
+  | {
+      type: "UPDATE_SELECTED_ELEMENT";
+      payload: EditorState["selectedElement"];
     };
 
 const initialElementId = nanoid();
 
-const initialEditorElement = {
+const initialEditorData: EditorData = {
   id: initialElementId,
-  content: "",
-  author: "",
-  backgroundColor: BackgroundColor[0],
-  fontSize: FontSize.md,
-  textAlign: TextAlign.left,
-  fontFamily: FontFamily.serif,
-  aspectRatio: AspectRatio.square,
+  template: {
+    type: null,
+    color: "#e4e4e4",
+  },
+  layout: {
+    backgroundColor: BackgroundColor[0],
+    aspectRatio: AspectRatio.square,
+  },
+  elements: [
+    {
+      type: "text",
+      content: {
+        id: "__text_content",
+        text: "",
+        fontSize: 16,
+        textAlign: TextAlign.left,
+        fontFamily: FontFamily.serif,
+      },
+    },
+    {
+      type: "text",
+      content: {
+        id: "__text_author",
+        text: "",
+        fontSize: 12,
+        textAlign: TextAlign.left,
+        fontFamily: FontFamily.serif,
+      },
+    },
+  ],
 };
 
 export const initalEditorState: Editor = {
-  config: { title: "무제" },
-  state: {
-    selectedElement: initialEditorElement,
+  config: {
+    title: "무제",
+    category: "etc",
+    tags: [],
+    isPublic: false,
   },
-  elements: [initialEditorElement],
+  state: {
+    selectedLayerId: initialElementId,
+    selectedElement: null,
+  },
+  data: [initialEditorData],
 };
 
 export const EditorValueContext = createContext<Editor>(initalEditorState);
@@ -94,15 +105,9 @@ export const EditorDispatchContext = createContext<
 //
 //
 interface EditorHandler {
-  quoteCardRef: RefObject<QuoteCardHandler | null> | null;
-  generateQuoteCardImage: () => void;
-  createQuoteCard: () => Promise<string | null>;
+  quoteCardRef: RefObject<HTMLDivElement | null> | null;
 }
 
 export const EditorHandlerContext = createContext<EditorHandler>({
   quoteCardRef: null,
-  generateQuoteCardImage: () => {},
-  createQuoteCard: async () => {
-    return null;
-  },
 });
--- src/app/editor/reducer.ts
+++ src/app/editor/reducer.ts
@@ -1,7 +1,12 @@
-import { Editor, EditorAction, EditorElement } from "./context";
+import { Editor } from "@/data/interfaces/editor/Editor";
+import { EditorAction } from "./context";
+import { EditorData } from "@/data/interfaces/editor";
 
-export const reducer = (editor: Editor, action: EditorAction) => {
+export const reducer = (editor: Editor, action: EditorAction): Editor => {
   switch (action.type) {
+    case "INIT_STATE":
+      return action.payload;
+
     case "UPDATE_CONFIG":
       return {
         ...editor,
@@ -11,27 +16,82 @@ export const reducer = (editor: Editor, action: EditorAction) => {
         },
       };
 
+    case "UPDATE_TEMPLATE":
+      const newEditorData = editor.data.map((data) => {
+        if (data.id === editor.state.selectedLayerId) {
+          const newData: EditorData = {
+            ...data,
+            template: {
+              ...data.template,
+              ...action.payload.template,
+            },
+          };
+          return newData;
+        } else return data;
+      });
+
+      return {
+        ...editor,
+        data: newEditorData,
+      };
+
+    case "UPDATE_SELECTED_ELEMENT":
+      return {
+        ...editor,
+        state: {
+          selectedLayerId: editor.state.selectedLayerId,
+          selectedElement: action.payload,
+        },
+      };
+
+    case "UPDATE_LAYOUT": {
+      const newEditorData = editor.data.map((data) => {
+        if (data.id === editor.state.selectedLayerId) {
+          const newData: EditorData = {
+            ...data,
+            layout: {
+              ...data.layout,
+              ...action.payload.layout,
+            },
+          };
+          return newData;
+        } else return data;
+      });
+
+      return {
+        ...editor,
+        data: newEditorData,
+      };
+    }
+
     case "UPDATE_ELEMENT": {
-      const newEditorData = editor.elements.map((element) => {
-        if (element.id !== action.payload.id) return element;
+      const newEditorData = editor.data.map((data) => {
+        if (data.id !== editor.state.selectedLayerId) return data;
         else {
-          const newElement: EditorElement = {
-            ...element,
-            ...action.payload,
+          const newElement: EditorData = {
+            ...data,
+            elements: data.elements.map((elem) => {
+              if (elem.content.id !== action.payload.element.content?.id)
+                return elem;
+
+              return {
+                ...elem,
+                ...action.payload.element,
+              };
+            }),
           };
 
           return newElement;
         }
       });
 
-      console.log({
-        ...editor,
-        elements: newEditorData,
-      });
-
       return {
         ...editor,
-        elements: newEditorData,
+        data: newEditorData,
+        state: {
+          ...editor.state,
+          selectedElement: action.payload.element,
+        },
       };
     }
 
