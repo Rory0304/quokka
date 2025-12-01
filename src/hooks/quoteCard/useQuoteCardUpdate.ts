5567878166387ef1c2a9dbce5df6e759f16a8388
Rory0304 <mitty0304@naver.com>
2025-12-02 05:34:55 +0900

    feat: add abort signal to quote card update hook

--- src/hooks/quoteCard/useQuoteCardUpdate.ts
+++ src/hooks/quoteCard/useQuoteCardUpdate.ts
@@ -1,13 +1,25 @@
 import { QuoteCardUpdateRequest } from "@/data/interfaces/request/quotecard/QuoteCardUpdateRequest";
 import { useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
 
 export const useQuoteCardUpdate = () => {
   return useMutation({
-    mutationFn: async (body: QuoteCardUpdateRequest) => {
+    mutationFn: async ({
+      body,
+      signal,
+    }: {
+      body: QuoteCardUpdateRequest;
+      signal?: AbortSignal;
+    }) => {
+      if (signal?.aborted) {
+        return;
+      }
+
       const response = await fetch("/api/quotecard", {
         method: "PATCH",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(body),
+        signal: signal,
       });
 
       return response.json();
@@ -16,7 +28,7 @@ export const useQuoteCardUpdate = () => {
       console.log("Todo created successfully!");
     },
     onError: (error) => {
-      console.error("Error creating todo:", error.message);
+      toast.error(`Error creating todo: ${error.message}`);
     },
   });
 };
