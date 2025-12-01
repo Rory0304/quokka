062f630c5972cd14f85295a775aaefac17acd0d3
Rory0304 <mitty0304@naver.com>
2025-12-02 05:15:42 +0900

    feat: add fetch options

--- src/libs/api/ApiFetch.ts
+++ src/libs/api/ApiFetch.ts
@@ -1,8 +1,10 @@
-export const ApiFetch = (url: string) => {
+export const ApiFetch = (url: string, options?: RequestInit) => {
   const baseUrl =
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
       : "http://localhost:3000";
 
-  return fetch(baseUrl + url);
+  return fetch(baseUrl + url, {
+    ...options,
+  });
 };
