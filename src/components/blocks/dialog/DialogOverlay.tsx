a46890c0cfe410ce59cf49c29b3989888894cdb1
Rory0304 <mitty0304@naver.com>
2025-12-02 05:17:28 +0900

    style: update dialog overlay

--- src/components/blocks/dialog/DialogOverlay.tsx
+++ src/components/blocks/dialog/DialogOverlay.tsx
@@ -3,6 +3,6 @@ import React, { FC } from "react";
 
 export const DialogOverlay: FC = () => {
   return (
-    <Dialog.Overlay className="fixed inset-0 bg-[#000000b3] data-[state=open]:animate-overlayShow" />
+    <Dialog.Overlay className="fixed top-0 left-0 z-10 inset-0 bg-[#000000b3] backdrop-blur-xs" />
   );
 };
