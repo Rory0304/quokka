70b3894b4c72932377ac29f0033bdbe528afbc3b
Rory0304 <mitty0304@naver.com>
2025-12-02 05:22:11 +0900

    feat: enhance QuoteCardCategory with music category and update display

--- src/components/pages/quoteCard/QuoteCardCategory.tsx
+++ src/components/pages/quoteCard/QuoteCardCategory.tsx
@@ -1,10 +1,14 @@
+import {
+  QuoteCardCategoryMap,
+  QuoteCardCategoryType,
+} from "@/data/constants/quoteCard/QuoteCardCategory";
 import React, { FC } from "react";
 
 export const QuoteCardCategory: FC<{ category: string }> = ({ category }) => {
   return (
     <div className="mb-2">
-      <span className="text-xs text-muted-foreground font-light">
-        {category}
+      <span className="text-xs text-gray-400 font-bold">
+        {QuoteCardCategoryMap[category as QuoteCardCategoryType]}
       </span>
     </div>
   );
--- src/data/constants/quoteCard/QuoteCardCategory.ts
+++ src/data/constants/quoteCard/QuoteCardCategory.ts
@@ -1,6 +1,7 @@
 export const QuoteCardCategory = {
   movie: "movie",
   book: "book",
+  music: "music",
   etc: "etc",
 } as const;
 
@@ -9,5 +10,6 @@ export type QuoteCardCategoryType = keyof typeof QuoteCardCategory;
 export const QuoteCardCategoryMap: Record<QuoteCardCategoryType, string> = {
   movie: "영화",
   book: "책",
+  music: "노래",
   etc: "기타",
 };
