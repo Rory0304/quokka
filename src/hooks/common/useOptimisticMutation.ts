e6e806c7cb3f2d146c4881c87cbb289f0c43d997
Rory0304 <mitty0304@naver.com>
2025-12-02 05:21:53 +0900

    feat: add optimistic update hook

---
+++ src/hooks/common/useOptimisticMutation.ts
@@ -0,0 +1,64 @@
+import {
+  type MutationFunction,
+  type QueryKey,
+  useMutation,
+  useQueryClient,
+} from "@tanstack/react-query";
+
+type OptimisticProps<
+  TData = unknown,
+  TVariables = void,
+  TQueryFnData = unknown
+> = {
+  queryKey: QueryKey;
+  invalidates: QueryKey[];
+  mutationFn: MutationFunction<TData, TVariables>;
+  updater: (
+    prevData: TQueryFnData | undefined,
+    variables: TVariables
+  ) => TQueryFnData | undefined;
+};
+
+export const useOptimisticMutation = <
+  TData = unknown,
+  TVariables = void,
+  TQueryFnData = unknown
+>({
+  queryKey,
+  invalidates,
+  mutationFn,
+  updater,
+}: OptimisticProps<TData, TVariables, TQueryFnData>) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn,
+    onMutate: async (variables) => {
+      await queryClient.cancelQueries({
+        queryKey,
+      });
+
+      const previousData = queryClient.getQueryData(queryKey);
+
+      queryClient.setQueryData(queryKey, (prevData: TQueryFnData | undefined) =>
+        updater(prevData, variables)
+      );
+
+      return () => {
+        queryClient.setQueryData(queryKey, previousData);
+      };
+    },
+    onError: (err, variables, rollback) => {
+      rollback?.();
+    },
+    onSettled: () => {
+      const fn = invalidates.map((invalidateKey) =>
+        queryClient.invalidateQueries({
+          queryKey: invalidateKey,
+        })
+      );
+
+      return Promise.all(fn);
+    },
+  });
+};
