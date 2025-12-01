9f3067efbe4a1f3b80555c3294813577cec8ce00
Rory0304 <mitty0304@naver.com>
2025-12-02 05:27:36 +0900

    feat: enhance image handling in editor actions with aspect ratio support and update quote card functionality

--- src/hooks/editor/useEditorAction.ts
+++ src/hooks/editor/useEditorAction.ts
@@ -9,95 +9,145 @@ import { useQuoteCardCreate } from "../quoteCard/useQuoteCardCreate";
 import { Editor } from "@/data/interfaces/editor/Editor";
 import { nanoid } from "nanoid";
 import { QuoteCardCreateRequest } from "@/data/interfaces/request/quotecard/QuoteCardCreateRequest";
+import { canvasToBlob } from "@/libs/canvas/canvasToBlob";
+import { toast } from "sonner";
+import { RouteConfig } from "@/data/constants/route";
+import { useRouter } from "next/navigation";
+import { useQuoteCardUpdate } from "../quoteCard/useQuoteCardUpdate";
+import { AspectRatioType } from "@/data/constants/editor/AspectRatio";
 
 export const useEditorAction = () => {
+  const router = useRouter();
   const uploadImageMutation = useUploadImage();
-  const createQuoteCardMutation = useQuoteCardCreate();
+  const createMutation = useQuoteCardCreate();
+  const updateMutation = useQuoteCardUpdate();
 
   const { quoteCardRef } = useContext(EditorHandlerContext);
 
-  const _getQuoteCardElement = useCallback(() => {
-    return quoteCardRef?.current || null;
-  }, []);
-
   const _getImageFileName = useCallback((id: string) => {
-    return `${id}.png`;
+    return `${id}.jpg`;
   }, []);
 
-  const _createImageFile = async (id: string): Promise<File> => {
-    const elem = _getQuoteCardElement();
-
-    if (elem === null) {
-      new Error(`id is not exist`);
+  const _getImageSize = (aspectRatio: AspectRatioType) => {
+    if (aspectRatio === "square") {
+      return {
+        windowWidth: 1200,
+        windowHeight: 1200,
+      };
     }
+
+    return {
+      windowWidth: 1200,
+      windowHeight: 1600,
+    };
+  };
+
+  const _createImageFile = async (
+    id: string,
+    aspectRatio: AspectRatioType
+  ): Promise<File> => {
+    const elem = quoteCardRef?.current;
 
     if (!elem) {
-      throw new Error(`id does not exist`);
+      throw new Error("Not found matched quote card element");
     }
 
-    const canvas = await html2canvas(elem, { scale: 2 });
-    const blob = await new Promise<Blob | null>((resolve) =>
-      canvas.toBlob((b) => resolve(b), "image/png")
-    );
-
-    if (!blob) {
-      throw new Error("Failed to convert canvas to blob");
-    }
+    const canvas = await html2canvas(elem, _getImageSize(aspectRatio));
+    const blob = await canvasToBlob(canvas, "image/jpg");
 
     const fileName = _getImageFileName(id);
     const file = new File([blob], fileName, {
-      type: "image/png",
+      type: "image/jpg",
     });
-
     return file;
   };
 
-  const _uploadImageFile = async (): Promise<string | null> => {
-    try {
-      const id = nanoid();
-      const file = await _createImageFile(id);
-      const result = await uploadImageMutation.mutateAsync({
-        storage: "CardThumbnail" as const,
-        file,
-      });
+  const _uploadImageFile = async (
+    aspectRatio: AspectRatioType
+  ): Promise<string> => {
+    const id = nanoid();
 
-      return result.publicUrl;
+    const file = await _createImageFile(id, aspectRatio);
+    const result = await uploadImageMutation.mutateAsync({
+      storage: "CardThumbnail" as const,
+      file,
+    });
+
+    return result.publicUrl;
+  };
+
+  const donwloadImage = async (id: string, aspectRatio: AspectRatioType) => {
+    try {
+      const file = await _createImageFile(id, aspectRatio);
+      const fileName = _getImageFileName(id);
+
+      saveAs(file, fileName);
     } catch (error) {
-      console.error("Error converting div to image:", error);
-      return null;
+      console.error(error);
+      toast.error("다운로드에 오류가 발생했습니다.", {});
     }
   };
 
-  const donwloadImage = async (id: string) => {
-    try {
-      const file = await _createImageFile(id);
-      const fileName = _getImageFileName(id);
-      saveAs(file, fileName);
-    } catch (error) {
-      console.error("Error converting div to image:", error);
+  const updateQuoteCard = async (editor: Editor) => {
+    if (editor.id) {
+      // 1. 이미지를 먼저 업로드
+      const thumbnailUrl = await _uploadImageFile(
+        editor.data[0].layout.aspectRatio
+      );
+
+      updateMutation.mutate({
+        body: {
+          id: editor.id,
+          data: {
+            title: editor.config.title,
+            category: editor.config.category,
+            tags: editor.config.tags,
+            isPublic: editor.config.isPublic,
+            customFields: {
+              data: editor.data,
+            },
+            thumbnailUrl,
+          },
+        },
+      });
     }
   };
 
   const createQuoteCard = async (editorData: Editor) => {
-    // 1. upload image first
-    const thumbnailUrl = (await _uploadImageFile()) ?? undefined;
-    const body: QuoteCardCreateRequest = {
-      title: editorData.config.title,
-      category: editorData.config.category,
-      tags: editorData.config.tags,
-      isPublic: editorData.config.isPublic,
-      customFields: {
-        data: editorData.data,
-      },
-      thumbnailUrl,
-    };
+    try {
+      // 1. 이미지를 먼저 업로드
+      const thumbnailUrl = await _uploadImageFile(
+        editorData.data[0].layout.aspectRatio
+      );
 
-    // 1. create quote card first
-    return createQuoteCardMutation.mutate(body);
+      // 2. public url 을 연동
+      const body: QuoteCardCreateRequest = {
+        title: editorData.config.title,
+        category: editorData.config.category,
+        tags: editorData.config.tags,
+        isPublic: editorData.config.isPublic,
+        customFields: {
+          data: editorData.data,
+        },
+        thumbnailUrl,
+      };
+
+      createMutation.mutate(body, {
+        onSuccess: () => {
+          toast.success("초대장이 생성되었습니다.");
+          router.push(RouteConfig.my);
+        },
+        onError: () => toast.error("업로드에 오류가 발생했습니다.", {}),
+      });
+    } catch (error) {
+      console.error(error);
+      toast.error("이미지 업로드에 실패했습니다.", {});
+    }
   };
 
   return {
     donwloadImage,
     createQuoteCard,
+    updateQuoteCard,
   };
 };
