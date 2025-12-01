91a71b02b490e6222bab2c4357979d75afef4ba2
Rory0304 <mitty0304@naver.com>
2025-12-02 05:21:33 +0900

    style: update switch ui

--- src/components/blocks/switch/Switch.tsx
+++ src/components/blocks/switch/Switch.tsx
@@ -1,33 +1,21 @@
 import React, { FC } from "react";
 import { Switch as RadixSwitch } from "radix-ui";
-import { ClassNameValue } from "tailwind-merge";
-import { cn } from "@/libs/styles/cn";
 
 interface SwitchProps {
   id: string;
-  label: string;
   checked: boolean;
-  className?: ClassNameValue;
+  onCheckedChange?: (checked: boolean) => void;
 }
 
-export const Switch: FC<SwitchProps> = ({ id, label, checked, className }) => {
+export const Switch: FC<SwitchProps> = ({ id, checked, onCheckedChange }) => {
   return (
-    <form>
-      <div className={cn("flex items-center", className)}>
-        <label
-          className="pr-4 text-sm font-medium leading-none text-foreground"
-          htmlFor={id}
-        >
-          {label}
-        </label>
-        <RadixSwitch.Root
-          className="relative h-[25px] w-[42px] rounded-full bg-gray-300 outline-none data-[state=checked]:bg-blue-500"
-          id={id}
-          checked={checked}
-        >
-          <RadixSwitch.Thumb className="block size-[21px] translate-x-0.5 rounded-full bg-white shadow-xl transition-transform duration-100 will-change-transform data-[state=checked]:translate-x-[19px]" />
-        </RadixSwitch.Root>
-      </div>
-    </form>
+    <RadixSwitch.Root
+      className="relative h-[25px] w-[42px] rounded-full bg-gray-300 outline-none data-[state=checked]:bg-blue-500"
+      id={id}
+      checked={checked}
+      onCheckedChange={onCheckedChange}
+    >
+      <RadixSwitch.Thumb className="block size-[21px] translate-x-0.5 rounded-full bg-white shadow-xl transition-transform duration-100 will-change-transform data-[state=checked]:translate-x-[19px]" />
+    </RadixSwitch.Root>
   );
 };
