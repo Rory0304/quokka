513ed8abe00cf2a87b456fa0f35f5fec4eb2579c
Rory0304 <mitty0304@naver.com>
2025-12-03 02:13:32 +0900

    feat: add auth tooltip

---
+++ src/components/blocks/tooltip/Tooltip.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { cn } from "@/libs/styles/cn";
+import * as TooltipPrimitive from "@radix-ui/react-tooltip";
+import * as React from "react";
+
+const TooltipProvider = TooltipPrimitive.Provider;
+
+const Tooltip = TooltipPrimitive.Root;
+
+const TooltipTrigger = TooltipPrimitive.Trigger;
+
+const TooltipContent = React.forwardRef<
+  React.ComponentRef<typeof TooltipPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>
+>(({ className, sideOffset = 7, ...props }, ref) => (
+  <TooltipPrimitive.Content
+    ref={ref}
+    sideOffset={sideOffset}
+    className={cn(
+      "z-50 overflow-hidden rounded-md bg-slate-100 px-3 py-1.5 text-xs text-muted-foreground",
+      className
+    )}
+    {...props}
+  />
+));
+TooltipContent.displayName = TooltipPrimitive.Content.displayName;
+
+export { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger };
