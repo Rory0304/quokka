f282d227167511a1b4d9e98a0361ecc286bd1d78
Rory0304 <mitty0304@naver.com>
2025-12-05 03:48:56 +0900

    feat: add navigation back route

--- src/components/pages/editor/navigation/EditorNavigation.tsx
+++ src/components/pages/editor/navigation/EditorNavigation.tsx
@@ -1,12 +1,20 @@
 import React, { FC } from "react";
 import { EditorActions } from "./EditorActions";
 import { EditorTitleInput } from "./EditorTitleInput";
+import { CaretLeftIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
+import { RouteConfig } from "@/data/constants/route";
 
 export const EditorNavigation: FC = () => {
   return (
     <div className="fixed z-10 w-full h-16 max-h-16">
       <div className="mx-auto bg-white border-b border-gray-300 px-4 flex items-center justify-between h-full">
-        <EditorTitleInput />
+        <div className="flex items-center gap-2">
+          <Link href={RouteConfig.my} aria-label="back to my page">
+            <CaretLeftIcon width={24} height={24} />
+          </Link>
+          <EditorTitleInput />
+        </div>
         <EditorActions />
       </div>
     </div>
