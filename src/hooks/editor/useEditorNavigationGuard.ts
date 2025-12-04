b881b9f9d4a0ace3350cbb1717787503ccbd80fb
Rory0304 <mitty0304@naver.com>
2025-12-05 02:22:37 +0900

    feat: add editor navigation guard

---
+++ src/hooks/editor/useEditorNavigationGuard.ts
@@ -0,0 +1,25 @@
+import { useNavgationGuard } from "../common";
+import { useEditor } from "./useEditor";
+
+/**
+ * 뒤로가기, 새로고침을 방지하는 훅
+ * - 폼이 변경되었을 경우: 활성화
+ * - 에디엍 저장/업데이트 후 리다이렉트하는 경우: 비활성화
+ */
+export const useEditorNavigationGuard = () => {
+  const {
+    editorState: { isDirty, isSaving },
+  } = useEditor();
+
+  const {
+    onCancel,
+    onDiscard,
+    active: openSaveModal,
+  } = useNavgationGuard({ enabled: isDirty && !isSaving });
+
+  return {
+    onCancel,
+    onDiscard,
+    openSaveModal,
+  };
+};
