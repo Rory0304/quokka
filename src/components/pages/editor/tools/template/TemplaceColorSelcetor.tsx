37850c84027459865fab6f18f7fba54d7e94524e
Rory0304 <mitty0304@naver.com>
2025-12-04 20:32:35 +0900

    feat: add template color selector

---
+++ src/components/pages/editor/tools/template/TemplaceColorSelcetor.tsx
@@ -0,0 +1,38 @@
+import { GradientColorPicker } from "@/components/blocks/colorPicker/GradientColorPicker";
+import { useEditor } from "@/hooks/editor/useEditor";
+import { FC, useState } from "react";
+
+export const TemplateColorSelcetor: FC = () => {
+  const { editorTemplate, dispatch } = useEditor();
+
+  const [open, setOpen] = useState(false);
+
+  const handleTemplateColorChange = (color: string) => {
+    dispatch({
+      type: "UPDATE_TEMPLATE",
+      payload: {
+        template: {
+          color: color,
+        },
+      },
+    });
+  };
+
+  return (
+    <div>
+      <button type="button" onClick={() => setOpen((current) => !current)}>
+        <div
+          className="w-6 h-6 rounded-md"
+          style={{
+            backgroundColor: editorTemplate?.color,
+          }}
+        />
+      </button>
+      <GradientColorPicker
+        open={open}
+        value={editorTemplate?.color}
+        onChange={handleTemplateColorChange}
+      />
+    </div>
+  );
+};
