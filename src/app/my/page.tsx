47fb8548e4a0c3423ba42f1529b67b3117da4b4a
Rory0304 <mitty0304@naver.com>
2025-12-05 03:48:20 +0900

    feat: add quote card create link

--- src/app/my/page.tsx
+++ src/app/my/page.tsx
@@ -3,8 +3,12 @@
 import { ErrorAlert } from "@/components/blocks/alert/ErrorAlert";
 import { AsyncBoundary } from "@/components/blocks/asyncBoundary/AsyncBoundary";
 import { GlobalLayout } from "@/components/blocks/global";
+import { MyQuoteCardItemLoading } from "@/components/pages/my/MyQuoteCardItemLoading";
 import { MyQuoteCardList } from "@/components/pages/my/MyQuoteCardList";
+import { RouteConfig } from "@/data/constants/route";
 import { useAuth } from "@/hooks/auth";
+import { PlusIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 export default function MyPage() {
   const { isLogin, status } = useAuth();
@@ -28,18 +32,29 @@ export default function MyPage() {
 
   return (
     <GlobalLayout>
-      <AsyncBoundary
-        pendingFallback={<div>Loading...</div>}
-        errorFallback={({ reset }) => (
-          <ErrorAlert
-            title="관련 정보를 찾지 못했어요"
-            description="잠시후 다시 시도해주세요"
-            onReset={reset}
-          />
-        )}
-      >
-        <MyQuoteCardList />
-      </AsyncBoundary>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10 group pt-12">
+        <Link href={RouteConfig.editor}>
+          <div className="w-full h-full bg-green-50 flex flex-col justify-center items-center rounded-xl py-6 text-muted-foreground">
+            <div className="flex items-center justify-center rounded-full bg-green-300 w-10 h-10 mb-4">
+              <PlusIcon className="text-white" width={20} height={20} />
+            </div>
+            <p className="font-semibold text-sm">인용 카드 생성하기</p>
+          </div>
+        </Link>
+
+        <AsyncBoundary
+          pendingFallback={<MyQuoteCardItemLoading count={5} />}
+          errorFallback={({ reset }) => (
+            <ErrorAlert
+              title="관련 정보를 찾지 못했어요"
+              description="잠시후 다시 시도해주세요"
+              onReset={reset}
+            />
+          )}
+        >
+          <MyQuoteCardList />
+        </AsyncBoundary>
+      </div>
     </GlobalLayout>
   );
 }
