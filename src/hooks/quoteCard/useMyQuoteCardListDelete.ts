a1419c447777c72ff1b95976a75c031c537e9de7
Rory0304 <mitty0304@naver.com>
2025-12-06 20:28:59 +0900

    feat: remove optimstic update logic

--- src/hooks/quoteCard/useMyQuoteCardListDelete.ts
+++ src/hooks/quoteCard/useMyQuoteCardListDelete.ts
@@ -1,17 +1,16 @@
 import { QueryKey } from "@/data/constants/querykey/QueryKey";
 import { EditorData } from "@/data/interfaces/editor";
 
-import { InfiniteData } from "@tanstack/react-query";
-
-import { useOptimisticMutation } from "../common";
-import { QuoteCardListResponse } from "@/data/interfaces/response/quotecard/QuoteCardListResponse";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 interface QuoteCardDeleteRequest {
   id: EditorData["id"];
 }
 
 export const useMyQuoteCardListDelete = () => {
-  const mutate = async (body: QuoteCardDeleteRequest) => {
+  const queryClient = useQueryClient();
+
+  const deleteQuoteCard = async (body: QuoteCardDeleteRequest) => {
     const response = await fetch("/api/quotecard", {
       method: "DELETE",
       headers: { "Content-Type": "application/json" },
@@ -21,32 +20,12 @@ export const useMyQuoteCardListDelete = () => {
     return response.json();
   };
 
-  return useOptimisticMutation<
-    QuoteCardListResponse,
-    QuoteCardDeleteRequest,
-    InfiniteData<QuoteCardListResponse>
-  >({
-    queryKey: [QueryKey.quoteCard.get_my_quotecard_list],
-    invalidates: [[QueryKey.quoteCard.get_my_quotecard_list]],
-    mutationFn: mutate,
-    updater: (prev, variables) => {
-      if (!prev) return prev;
-
-      const pages = prev.pages.map((page) => {
-        const data = page.data;
-        const pagination = page.pagination;
-        const newData = data.filter((item) => item.id !== variables.id);
-
-        return {
-          data: newData,
-          pagination,
-        };
+  return useMutation({
+    mutationFn: deleteQuoteCard,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QueryKey.quoteCard.get_my_quotecard_list],
       });
-
-      return {
-        pageParams: prev.pageParams,
-        pages: pages,
-      };
     },
   });
 };
