31a7850fd15fbdf3ff09561843368d061b33037a
Rory0304 <mitty0304@naver.com>
2025-12-06 21:00:29 +0900

    feat: fix domain

--- src/libs/api/ApiFetch.ts
+++ src/libs/api/ApiFetch.ts
@@ -2,7 +2,7 @@ export const ApiFetch = (url: string, options?: RequestInit) => {
   const baseUrl =
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
-      : "http://localhost:3000";
+      : "https://quokka-io.vercel.app";
 
   return fetch(baseUrl + url, {
     ...options,
