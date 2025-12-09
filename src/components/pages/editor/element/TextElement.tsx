9a286c6463f72a9aa9e3fa303dd3d6bbe5afd0fc
Rory0304 <mitty0304@naver.com>
2025-12-10 04:26:53 +0900

    feat: handle dompurify

--- src/components/pages/editor/element/TextElement.tsx
+++ src/components/pages/editor/element/TextElement.tsx
@@ -2,8 +2,11 @@ import {
   FontFamilyMap,
   FontFamilyType,
 } from "@/data/constants/editor/FontFamily";
+import { cn } from "@/libs/styles/cn";
 import React, { FC, useRef } from "react";
 import ContentEditable, { ContentEditableEvent } from "react-contenteditable";
+import { ClassNameValue } from "tailwind-merge";
+import DOMPurify from "dompurify";
 
 interface TextElementProps {
   fontSize: number;
@@ -12,6 +15,7 @@ interface TextElementProps {
   fontFamily: FontFamilyType;
   placeholder?: string;
   color?: string;
+  className?: ClassNameValue;
   onClick: () => void;
   onChange?: (text: string) => void;
 }
@@ -24,11 +28,12 @@ export const TextElement: FC<TextElementProps> = (props) => {
     placeholder,
     color = "#000",
     defaultValue,
+    className,
     onChange,
     onClick,
   } = props;
 
-  const textRef = useRef(defaultValue || "");
+  const textRef = useRef(DOMPurify.sanitize(defaultValue || ""));
 
   const handleChange = (event: ContentEditableEvent) => {
     const htmlValue = event.target.value;
@@ -39,22 +44,25 @@ export const TextElement: FC<TextElementProps> = (props) => {
 
     // 빈 텍스트인 경우 빈 문자열로 설정 (HTML도 빈 문자열로)
     const normalizedValue = textContent.trim() === "" ? "" : htmlValue;
+    const sanitizedValue = DOMPurify.sanitize(normalizedValue);
 
-    textRef.current = normalizedValue;
-    onChange?.(normalizedValue);
+    textRef.current = sanitizedValue;
+    onChange?.(sanitizedValue);
   };
 
   const handlePaste = (event: React.ClipboardEvent<HTMLInputElement>) => {
     event.preventDefault();
     const targetElement = event.target as HTMLElement; // or HTMLButtonElement, HTMLInputElement, etc.
     if (targetElement) {
-      targetElement.innerText = event.clipboardData.getData("text/plain");
+      targetElement.innerText = DOMPurify.sanitize(
+        event.clipboardData.getData("text/plain")
+      );
     }
   };
 
   return (
     <ContentEditable
-      className="content-editable-placeholder overlfow-scroll"
+      className={cn("content-editable-placeholder overlfow-scroll", className)}
       style={{
         color,
         marginBottom: "4px",
