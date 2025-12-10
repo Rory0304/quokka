d05d135f52cd27a994b58483d5628d2dccda4cd9
Rory0304 <mitty0304@naver.com>
2025-12-10 20:44:23 +0900

    feat: add Visibility component to manage element visibility using react-intersection-observer

---
+++ src/components/blocks/visibility/Visibility.tsx
@@ -0,0 +1,15 @@
+import React, { ComponentProps, FC } from "react";
+import { InView } from "react-intersection-observer";
+
+type InViewProps = Omit<ComponentProps<typeof InView>, "children">;
+type VisibilityProps = InViewProps & {
+  children?: React.ReactNode;
+};
+
+export const Visibility: FC<VisibilityProps> = ({ children, ...props }) => {
+  return (
+    <InView as="div" {...props}>
+      {children}
+    </InView>
+  );
+};
