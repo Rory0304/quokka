1046dd5d3f9f87a41cf92f5acdb5b6b40a4d8729
Rory0304 <mitty0304@naver.com>
2025-12-10 20:46:35 +0900

    feat: implement onEndReached callback for infinite scrolling in bookmark and quote card hooks

--- src/hooks/quoteCard/useQuoteCardList.ts
+++ src/hooks/quoteCard/useQuoteCardList.ts
@@ -3,20 +3,24 @@ import { QuoteCardListResponse } from "@/data/interfaces/response/quotecard/Quot
 import { useSuspenseInfiniteQuery } from "@tanstack/react-query";
 import qs from "query-string";
 import { QuoteCardCategoryType } from "@/data/constants/quoteCard/QuoteCardCategory";
+import { useCallback } from "react";
+import { SortBy } from "@/data/interfaces/sortBy/SortBy";
 
 interface UseQuoteCardListOptions {
   category?: QuoteCardCategoryType;
   searchKey?: string;
   limit?: number;
+  sort?: SortBy;
 }
 
 export const useQuoteCardList = (options?: UseQuoteCardListOptions) => {
-  const { category, searchKey, limit = 10 } = options || {};
+  const { category, searchKey, limit = 10, sort = "desc" } = options || {};
 
   const fetchItems = async ({ pageParam }: { pageParam: number | null }) => {
     const query = qs.stringify({
       cursor: pageParam ?? undefined,
       limit,
+      sort,
       category: category ?? undefined,
       searchKey: searchKey?.trim() || undefined,
     });
@@ -31,18 +35,34 @@ export const useQuoteCardList = (options?: UseQuoteCardListOptions) => {
     return data as QuoteCardListResponse;
   };
 
-  const { data, isSuccess, hasNextPage, isFetchingNextPage } =
-    useSuspenseInfiniteQuery({
-      queryKey: [QueryKey.quoteCard.get_quotecard_list, category, searchKey],
-      initialPageParam: null,
-      queryFn: ({ pageParam }: { pageParam: number | null }) =>
-        fetchItems({ pageParam }),
-      getNextPageParam: (lastPage: QuoteCardListResponse) =>
-        lastPage.pagination.nextCursor,
-    });
+  const {
+    data,
+    isSuccess,
+    hasNextPage,
+    isError,
+    isFetchingNextPage,
+    fetchNextPage,
+  } = useSuspenseInfiniteQuery({
+    queryKey: [
+      QueryKey.quoteCard.get_quotecard_list,
+      category,
+      searchKey,
+      sort,
+    ],
+    initialPageParam: null,
+    queryFn: ({ pageParam }: { pageParam: number | null }) =>
+      fetchItems({ pageParam }),
+    getNextPageParam: (lastPage: QuoteCardListResponse) =>
+      lastPage.pagination.nextCursor,
+  });
 
   const list = data?.pages.flatMap((page) => page.data) ?? [];
   const isEmpty = isSuccess && list.length === 0;
+
+  const onEndReached = useCallback(() => {
+    if (isFetchingNextPage || !hasNextPage || isError) return;
+    fetchNextPage();
+  }, [isFetchingNextPage, hasNextPage, isError, fetchNextPage]);
 
   return {
     list,
@@ -50,5 +70,7 @@ export const useQuoteCardList = (options?: UseQuoteCardListOptions) => {
     isFetchingNextPage,
     isEmpty,
     hasNextPage,
+    fetchNextPage,
+    onEndReached,
   };
 };
