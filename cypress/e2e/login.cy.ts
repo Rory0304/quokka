75af10889459c9ed22a40f9fd3b02a7daf34b855
Rory0304 <mitty0304@naver.com>
2025-12-13 20:35:13 +0900

    feat: add login test

---
+++ cypress/e2e/login.cy.ts
@@ -0,0 +1,18 @@
+describe('Cypress login', () => {
+  it('should provide a valid session', () => {
+    // Login command sets up the session intercept and cookie
+    cy.login();
+    // Visit a route in order to allow cypress to actually set the cookie
+    cy.visit('/');
+    // Wait until the intercepted request is ready
+    cy.wait('@session');
+    // This is where you can now add assertions
+    // Example: provide a data-test-id on an element.
+    // This can be any selector that "always and only" exists when the user is logged in
+    cy.get("[data-id='authenticated']")
+      .should('exist')
+      .then(() => {
+        cy.log('Cypress login successful');
+      });
+  });
+});
