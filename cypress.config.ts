8207f44fb698d8881452ecf445ead6d454e60fa9
Rory0304 <mitty0304@naver.com>
2025-12-13 20:39:27 +0900

    chore: remove plugin integration from Cypress configuration for simplified setup

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,10 @@
 import { defineConfig } from 'cypress';
 
-import plugin from './cypress/plugins/index';
-
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000/',
     chromeWebSecurity: false,
     setupNodeEvents(on, config) {
-      plugin(on, config);
       return config;
     },
   },
