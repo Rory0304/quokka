10e25ee9ecf1d3d0b70206f826e7c90252436965
Rory0304 <mitty0304@naver.com>
2025-12-14 19:11:08 +0900

    feat: add cypress login

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -5,6 +5,7 @@ describe('Cypress login', () => {
     // Visit a route in order to allow cypress to actually set the cookie
     cy.visit('/');
     // Wait until the intercepted request is ready
+    // /api/auth/session 으로 세션 확인하는 요청이 완료될 때까지 대기
     cy.wait('@session');
     // This is where you can now add assertions
     // Example: provide a data-test-id on an element.
